c4ce099a5452c08dfd14b51fc3f06a680a51e5fc
Harshith Kumar M <harshithkumarm6@gmail.com>
2026-01-03 20:40:03 +0530

    DOM

---
+++ DOM/scr.js
@@ -0,0 +1,8 @@
+let div = document.querySelector("div");
+console.dir(div)
+console.log(div);
+let head = document.getElementsByTagName("h1");
+console.dir(head);
+let head1 = document.querySelector("h1")
+console.dir(head1)
+
